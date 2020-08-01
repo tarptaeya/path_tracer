c6a91a591e3aa5e1b82000bb65219c9fd7279a23
Anmol Gautam <tarptaeya@gmail.com>
2020-08-01 14:35:22 +0530

    added antialiasing to scene#render

--- lib/ray_tracer/scene.rb
+++ lib/ray_tracer/scene.rb
@@ -1,12 +1,13 @@
 module RayTracer
   class Scene
-    def initialize(width, height, camera)
+    def initialize(width, height, camera, world)
       @width = width
       @height = height
       @camera = camera
+      @world = world
     end
 
-    def render(file="output.ppm")
+    def render(ns=1, file="output.ppm")
       File.open(file, "w") do |f|
         f << "P3\n"
         f << "#{@width} #{@height}\n"
@@ -14,11 +15,16 @@ module RayTracer
 
         (0...@height).reverse_each do |y|
           (0...@width).each do |x|
-            i = x / @width.to_f
-            j = y / @height.to_f
 
-            ray = @camera.ray(i, j)
-            col = color(ray)
+            col = Vector[0, 0, 0]
+            (1..ns).each do
+              i = (x + Random.rand) / @width.to_f
+              j = (y + Random.rand) / @height.to_f
+              ray = @camera.ray(i, j)
+              col += color(ray)
+            end
+            col /= ns.to_f
+
 
             r = (255.99 * col[0]).truncate
             g = (255.99 * col[1]).truncate
@@ -33,8 +39,7 @@ module RayTracer
     private
 
     def color(ray)
-      sphere = Sphere.new(Vector[0, 0, 0], 0.5)
-      if sphere.hit(ray, 0.001, Float::INFINITY)
+      if @world.hit(ray, 0.001, Float::INFINITY)
         Vector[1, 0, 0]
       else
         Vector[1, 1, 1]
