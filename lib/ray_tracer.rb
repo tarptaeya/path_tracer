e8d25f5d22d248bae8c8c1ab1623bb2b786cb48d
Anmol Gautam <tarptaeya@gmail.com>
2020-08-01 08:08:58 +0530

    added ray class

---
+++ lib/ray_tracer.rb
@@ -0,0 +1,2 @@
+module RayTracer
+end
---
+++ lib/ray_tracer/ray.rb
@@ -0,0 +1,14 @@
+module RayTracer
+  class Ray
+    attr_reader :origin, :direction
+
+    def initialize(origin, direction)
+      @origin = origin
+      @direction = direction
+    end
+
+    def point(t)
+      @origin + t * @direction
+    end
+  end
+end
