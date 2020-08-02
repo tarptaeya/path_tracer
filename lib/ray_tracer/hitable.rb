f045a9e749e3432c30b09e308f470f30341c31b6
Anmol Gautam <tarptaeya@gmail.com>
2020-08-02 08:39:01 +0530

    added bvh

--- lib/ray_tracer/hitable.rb
+++ lib/ray_tracer/hitable.rb
@@ -6,8 +6,36 @@ module RayTracer
 end
 
 module RayTracer
+  class AABB
+    attr_reader :min, :max
+
+    def initialize(min, max)
+      @min = min
+      @max = max
+    end
+
+    def hit(ray, t_min, t_max)
+      for i in 0..2
+        inv_d = 1.0 / ray.direction[i]
+        t0 = (@min[i] - ray.origin[i]) * inv_d
+        t1 = (@max[i] - ray.origin[i]) * inv_d
+        t0, t1 = t1, t0 if inv_d < 0
+        t_min = t0 > t_min ? t0 : t_min
+        t_max = t1 < t_max ? t1 : t_max
+        return false if t_max <= t_min
+      end
+
+      true
+    end
+  end
+end
+
+module RayTracer
   class Hitable
     def hit(ray, t_min, t_max)
+    end
+
+    def bounding_box
     end
   end
 end
@@ -43,6 +71,13 @@ module RayTracer
       end
 
       nil
+    end
+
+    def bounding_box
+      min = @center - @radius * Vector[1, 1, 1]
+      max = @center + @radius * Vector[1, 1, 1]
+      @aabb ||= AABB.new(min, max)
+      @aabb
     end
 
     private
@@ -82,6 +117,80 @@ module RayTracer
       end
 
       rec
+    end
+  end
+end
+
+module RayTracer
+  class BVHNode < Hitable
+    def initialize(left, right)
+      @left = left
+      @right = right
+
+      if right
+        b0 = left.bounding_box
+        b1 = right.bounding_box
+        min = Vector[
+          [b0.min[0], b1.min[0]].min,
+          [b0.min[1], b1.min[1]].min,
+          [b0.min[2], b1.min[2]].min,
+        ]
+        max = Vector[
+          [b0.max[0], b1.max[0]].max,
+          [b0.max[1], b1.max[1]].max,
+          [b0.max[2], b1.max[2]].max,
+        ]
+        @aabb = AABB.new(min, max)
+      else
+        @aabb = left.bounding_box
+      end
+    end
+
+    def self.from_list(list)
+      n = list.count
+      axis = (3 * Random.rand).truncate
+      list.sort_by! {|x| x.bounding_box.min[axis]}
+
+      if n == 0
+        nil
+      elsif n == 1
+        BVHNode.new(list.first, nil)
+      elsif n == 2
+        BVHNode.new(list.first, list.last)
+      else
+        left = from_list(list.take(n / 2))
+        right = from_list(list.drop(n / 2))
+        BVHNode.new(left, right)
+      end
+    end
+
+    def hit(ray, t_min, t_max)
+      if @aabb.hit(ray, t_min, t_max)
+        t = t_max
+        rec = nil
+        left_hit = false
+        right_hit = false
+
+        if @left and  rec1 = @left.hit(ray, t_min, t)
+          left_hit = true
+          rec = rec1
+          t = rec.t
+        end
+
+        if @right and rec1 = @right.hit(ray, t_min, t)
+          right_hit = true
+          rec = rec1
+          t = rec.t
+        end
+
+        return rec if left_hit or right_hit
+      end
+
+      nil
+    end
+
+    def bounding_box
+      @aabb
     end
   end
 end
