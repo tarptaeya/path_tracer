15f44f2bc7c0d314c594b01b0e5afd0d240ad8b9
Anmol Gautam <tarptaeya@gmail.com>
2020-08-05 19:40:17 +0530

    added dielectric

--- lib/path_tracer/material.rb
+++ lib/path_tracer/material.rb
@@ -12,6 +12,17 @@ module PathTracer
     def reflect(v, n)
       v - 2 * v.dot(n) * n
     end
+
+    def refract(i, n, ni_over_nt)
+      cos_i = -i.dot(n)
+      cos_r_squared = 1 - (ni_over_nt) ** 2 * (1 - cos_i ** 2)
+      if cos_r_squared > 0
+        cos_r = cos_r_squared ** 0.5
+        ni_over_nt * i + (ni_over_nt * cos_i - cos_r) * n
+      else
+        nil
+      end
+    end
   end
 end
 
@@ -41,6 +52,32 @@ module PathTracer
       reflected = reflect(ray.direction.normalize, rec.n)
       scattered = Ray.new(rec.p, reflected + @fuzz * random_in_unit_sphere)
       if scattered.direction.dot(rec.n) > 0
+        attenuation = @albedo.value(rec.u, rec.v, rec.p)
+        [attenuation, scattered]
+      else
+        nil
+      end
+    end
+  end
+end
+
+module PathTracer
+  class Dielectric < Material
+    def initialize(refractive_index, albedo=ConstTexture.new(Vector[1, 1, 1]))
+      @refractive_index = refractive_index
+      @albedo = albedo
+    end
+
+    def scatter(ray, rec)
+      outer_normal = rec.n
+      ni_over_nt = 1.0 / @refractive_index
+      if ray.direction.dot(rec.n) > 0
+        outer_normal *= -1.0
+        ni_over_nt = 1.0 / ni_over_nt
+      end
+
+      if refracted = refract(ray.direction, outer_normal, ni_over_nt)
+        scattered = Ray.new(rec.p, refracted)
         attenuation = @albedo.value(rec.u, rec.v, rec.p)
         [attenuation, scattered]
       else
