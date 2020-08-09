a7ed693fa5fe12ba9d8e89da26ee969011d94a5a
Anmol Gautam <tarptaeya@gmail.com>
2020-08-09 18:10:45 +0530

    updated cover for dielectric

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 Path tracer
 ---
 <img src="https://github.com/Tarptaeya/ray_tracer/blob/master/images/cover.jpg" width="100%" height="auto">
+```
+$ time ruby cover.rb
+writing cover.ppm
+[====================] 100%
 
+real	10m12.298s
+user	39m22.258s
+sys	0m8.533s
+```
 ### Features
 - BVH acceleration
 - OBJ triangle mesh loading
--- examples/cover.rb
+++ examples/cover.rb
@@ -5,17 +5,19 @@ include PathTracer
 width = 600
 height = 200
 
-checker = GroundCheckerTexture.new(Vector[0.2, 0.2, 0.2], Vector[0.9, 0.9, 0.9], 10)
+checker = GroundCheckerTexture.new(Vector[0.2, 0.2, 0.2], Vector[0.9, 0.9, 0.9], 5)
 environment_texture = ImageTexture.new(File.join(__dir__, "environment_1.jpg"))
 
-cam = Camera.new(Vector[0, 0, 2], Vector[0, 0, 0], Vector[0, 1, 0], 60, width / height.to_f, 0.2, 2.0)
+lookfrom = Vector[0, 1, 4]
+lookat = Vector[0, 1, 0]
+cam = Camera.new(lookfrom, lookat, Vector[0, 1, 0], 50, width / height.to_f, 0.2, (lookfrom - lookat).magnitude)
 world = BVHNode.from_list([
-  Sphere.new(Vector[-1, 0, 0], 0.5, Metal.new(ConstTexture.new(Vector[0.3, 0.3, 0.8]), 0.3)),
-  Sphere.new(Vector[0, 0, 0], 0.5, Metal.new(ConstTexture.new(Vector[0.9, 0.9, 0.9]), 0)),
-  Sphere.new(Vector[1, 0, 0], 0.5, Lambertian.new(ConstTexture.new(Vector[0.3, 0.8, 0.3]))),
+  Sphere.new(Vector[-2, 1, 0], 1.0, Metal.new(ConstTexture.new(Vector[0.95, 0.95, 0.95]), 0)),
+  Sphere.new(Vector[0, 1, 0], 1.0, Dielectric.new(1.5)),
+  Sphere.new(Vector[2, 1, 0], 1.0, Lambertian.new(ConstTexture.new(Vector[0.3, 0.8, 0.3]))),
 
-  Ground.new(-0.5, Lambertian.new(checker)),
+  Ground.new(0.0, Lambertian.new(checker)),
   Sphere.new(Vector[0, 0, 0], 1000, DiffuseLight.new(environment_texture)),
 ])
 scene = Scene.new(width, height, cam, world)
-scene.render(ns=1, "cover.ppm")
+scene.render(ns=10, "cover.ppm")
