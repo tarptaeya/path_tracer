b624676f3d892d233b1e11b95cac5c93e8f5a6ec
Anmol Gautam <tarptaeya@gmail.com>
2020-08-09 17:32:45 +0530

    added environment_1.jpg file to examples

--- examples/cover.rb
+++ examples/cover.rb
@@ -6,6 +6,7 @@ width = 600
 height = 200
 
 checker = GroundCheckerTexture.new(Vector[0.2, 0.2, 0.2], Vector[0.9, 0.9, 0.9], 10)
+environment_texture = ImageTexture.new(File.join(__dir__, "environment_1.jpg"))
 
 cam = Camera.new(Vector[0, 0, 2], Vector[0, 0, 0], Vector[0, 1, 0], 60, width / height.to_f, 0.2, 2.0)
 world = BVHNode.from_list([
@@ -14,7 +15,7 @@ world = BVHNode.from_list([
   Sphere.new(Vector[1, 0, 0], 0.5, Lambertian.new(ConstTexture.new(Vector[0.3, 0.8, 0.3]))),
 
   Ground.new(-0.5, Lambertian.new(checker)),
-  Sphere.new(Vector[0, 0, 0], 1000, DiffuseLight.new( ImageTexture.new("env1.jpg") )),
+  Sphere.new(Vector[0, 0, 0], 1000, DiffuseLight.new(environment_texture)),
 ])
 scene = Scene.new(width, height, cam, world)
-scene.render(ns=10, "cover.ppm")
+scene.render(ns=1, "cover.ppm")
